50c79816ff001a29249da3dd3bab0c9d328bd3c6
Maksim <Maksim>
2023-12-08 11:36:46 +0300

    Add files

---
+++ src/model/Accessories.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Accessories extends Product{
+
+    public Accessories(long id, String name, Double weight, Float size) {
+        super(id, name, weight, size);
+    }
+}
---
+++ src/model/Shoes.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Shoes extends Product{
+
+    public Shoes(long id, String name, Double weight, Float size) {
+        super(id, name, weight, size);
+    }
+}
